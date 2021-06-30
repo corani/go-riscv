9c05fd7f137ea70960c31bc97adea66c62f56cae
Daniel Bos <daniel.bos@sap.com>
2021-06-30 09:55:19 +0800

    samples: use Makefile

--- sample/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/opt/riscv/bin/riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 -static -nostdlib -Wl,--no-relax -o hello hello.S
-/opt/riscv/bin/riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 -static -nostdlib -Wl,--no-relax -o greet greet.S
